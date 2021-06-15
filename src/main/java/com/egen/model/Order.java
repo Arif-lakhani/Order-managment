5af1423e10672c9026ded406ed03c7d040f62c8a
nikunjsarda <nikunjsarda123@gmail.com>
2021-06-15 02:41:03 -0400

    Order_update

--- src/main/java/com/egen/model/Order.java
+++ src/main/java/com/egen/model/Order.java
@@ -1,20 +1,19 @@
 package com.egen.model;
 //
 
+import java.util.List;
+
 public class Order {
 
     private String id;
     private String status;
     private String order_customer_id;
-    private String order_item_name;
-    private String order_item_qty;
-    private String order_subtotal;
+    private List<Product> products;
+    private double order_subtotal;
     private String order_tax;
     private String order_shipping_charges;
     private String order_total;
-    private String order_payment_method;
-    private String order_payment_date;
-    private String order_payment_confirmation_number;
+    private List<Payment> payment;
     private Address order_billing_address;
     private Address order_shipping_address;
 
@@ -29,22 +28,18 @@ public class Order {
     public void setId(String id) {
         this.id = id;
     }
-    public Order(String status, String order_customer_id, String order_item_name,
-                 String order_item_qty, String order_subtotal, String order_tax,
-                 String order_shipping_charges, String order_total, String order_payment_method,
-                 String order_payment_date, String order_payment_confirmation_number,
-                 Address order_billing_address,Address order_shipping_address) {
+
+    public Order(String status, String order_customer_id, List<Product> products, double order_subtotal, String order_tax,
+                 String order_shipping_charges, String order_total, List<Payment> payment,
+                 Address order_billing_address, Address order_shipping_address) {
         this.status = status;
         this.order_customer_id = order_customer_id;
-        this.order_item_name = order_item_name;
-        this.order_item_qty = order_item_qty;
+        this.products = products;
         this.order_subtotal = order_subtotal;
         this.order_tax = order_tax;
         this.order_shipping_charges = order_shipping_charges;
         this.order_total = order_total;
-        this.order_payment_method = order_payment_method;
-        this.order_payment_date = order_payment_date;
-        this.order_payment_confirmation_number = order_payment_confirmation_number;
+        this.payment = payment;
         this.order_billing_address = order_billing_address;
         this.order_shipping_address = order_shipping_address;
     }
@@ -61,27 +56,19 @@ public class Order {
         this.order_customer_id = order_customer_id;
     }
 
-    public String getOrderItemName() {
-        return order_item_name;
+    public List<Product> getOrderProducts() {
+        return products;
     }
 
-    public void setOrderItemName(String order_item_name) {
-        this.order_item_name = order_item_name;
+    public void setOrderProducts(List<Product> products) {
+        this.products = products;
     }
 
-    public String getOrderItemQty() {
-        return order_item_qty;
-    }
-
-    public void setOrderItemQty(String order_item_qty) {
-        this.order_item_qty = order_item_qty;
-    }
-
-    public String getOrderSubtotal() {
+    public double getOrderSubtotal() {
         return order_subtotal;
     }
 
-    public void setOrderSubtotal(String order_subtotal) {
+    public void setOrderSubtotal(double order_subtotal) {
         this.order_subtotal = order_subtotal;
     }
 
@@ -100,38 +87,22 @@ public class Order {
     public void setOrderShippingCharges(String order_shipping_charges) {
         this.order_shipping_charges = order_shipping_charges;
     }
-
     public String getOrder_total() {
         return order_total;
+    }
+
+    public List<Payment> getPayment() {
+        return payment;
+    }
+
+    public void setPayment(List<Payment> payment) {
+        this.payment = payment;
     }
 
     public void setOrder_total(String order_total) {
         this.order_total = order_total;
     }
 
-    public String getOrder_payment_method() {
-        return order_payment_method;
-    }
-
-    public void setOrder_payment_method(String order_payment_method) {
-        this.order_payment_method = order_payment_method;
-    }
-
-    public String getOrder_payment_date() {
-        return order_payment_date;
-    }
-
-    public void setOrder_payment_date(String order_payment_date) {
-        this.order_payment_date = order_payment_date;
-    }
-
-    public String getOrder_payment_confirmation_number() {
-        return order_payment_confirmation_number;
-    }
-
-    public void setOrder_payment_confirmation_number(String order_payment_confirmation_number) {
-        this.order_payment_confirmation_number = order_payment_confirmation_number;
-    }
 
     public Address getOrder_billing_address() {
         return order_billing_address;
---
+++ src/main/java/com/egen/model/Product.java
@@ -0,0 +1,77 @@
+package com.egen.model;
+
+public class Product {
+
+    private String productId;
+    private String productName;
+    private int quantity;
+    private double tax;
+    private double price;
+    private double subTotalWithoutTax;
+    private double subTotalTax;
+
+    public Product() {}
+
+    public Product(String productId, String productName, int quantity, double tax, double price) {
+        super();
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+        this.tax = tax;
+        this.subTotalWithoutTax = this.price * this.quantity;
+        this.subTotalTax = this.tax / 100 * this.subTotalWithoutTax;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        this.subTotalWithoutTax = this.price * this.quantity;
+        this.subTotalTax = this.tax / 100 * this.subTotalWithoutTax;
+    }
+    public double getTax() {
+        return tax;
+    }
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+        this.subTotalWithoutTax = this.price * this.quantity;
+        this.subTotalTax = this.subTotalWithoutTax + this.tax / 100 * this.subTotalWithoutTax;
+    }
+    public double getSubtotalTax() {
+        return subTotalTax;
+    }
+
+
+    public static double sum(Product a, Product b) {
+        return a.getSubtotalTax() + b.getSubtotalTax();
+    }
+
+    @Override
+    public String toString() {
+        return "product [productId=" + productId + ", productName=" + productName + ", quantity=" + quantity + ", price=" + price
+                + ", subtotal=" + subTotalTax + "]";
+    }
+
+
+
+}
